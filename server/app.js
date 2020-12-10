f5214b8b64bd7bfa5b45d9f61a5091d313d366bf
Guillaume GAUVAIN <guillaume.gauvain@epitech.eu>
2020-12-10 16:41:53 +0100

    divide back and front - add file in react

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hi!')
+})
+
+app.listen(3000, () => console.log('Server ready'))
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('Process terminated')
+  })
+})
