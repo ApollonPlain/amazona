0ef8609666d8ff97aae2ea1f3e7cad24147f4e88
Guillaume GAUVAIN <guillaume.gauvain@epitech.eu>
2020-12-18 14:57:33 +0100

    Init begin of API

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
-const express = require('express')
+import express from 'express';
+import data from './data.js';
+
+// const express = require('express')
 
 const app = express()
 app.use(express.json())
+
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hi!')
 })
 
-app.listen(3000, () => console.log('Server ready'))
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+})
+
+app.listen(port, () => console.log(`Server ready at http://localhost:${port}`));
 
 process.on('SIGTERM', () => {
   server.close(() => {
