c332a531dffc081f07f8a388ec9c117bca948057
Guillaume GAUVAIN <guillaume.gauvain@epitech.eu>
2020-12-21 00:03:42 +0100

    redux product screen and call api

--- server/app.js
+++ server/app.js
@@ -1,25 +1,29 @@
 import express from 'express';
 import data from './data.js';
 
-// const express = require('express')
-
 const app = express()
 app.use(express.json())
+
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hi!')
+	res.send('Hi!')
 })
 
 app.get('/api/products', (req, res) => {
-  res.send(data.products);
+	res.send(data.products);
 })
+
+app.get('/api/products/:id', (req, res) => {
+	const product = data.products.find((x) => x._id === req.params.id);
+	if (product) {
+		res.send(product);
+	} else {
+		res.status(404).send({ message: 'Product not Found' });
+	}
+})
+
+
 
 app.listen(port, () => console.log(`Server ready at http://localhost:${port}`));
-
-process.on('SIGTERM', () => {
-  server.close(() => {
-    console.log('Process terminated')
-  })
-})
