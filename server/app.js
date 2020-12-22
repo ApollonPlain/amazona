2050c9ffc72223c879360d3400abda7a4bdec7a6
Guillaume GAUVAIN <guillaume.gauvain@epitech.eu>
2020-12-22 11:42:17 +0100

    mongodb + model user + route

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './api-routes/userRouter.js';
 import data from './data.js';
+
 
 const app = express()
 app.use(express.json())
 
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
 
-const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.send('Hi!')
@@ -25,5 +32,13 @@ app.get('/api/products/:id', (req, res) => {
 })
 
 
+app.use('/api/users', userRouter);
+
+
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });
+})
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server ready at http://localhost:${port}`));
